48bb5de3a0bbf67e19d7d2b7f74ca6a9565fb8b9
MaribelC <Maribel_Calvario@yahoo.com>
2023-09-25 22:37:39 -0500

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# PracticeCalendar
+# Work Day Calendar
+
+## Description
+As a full stack web developer, I want to create a daily planner for an employee with a busy schedule to add important events so that he/she can manage his/her time effectively.
+
+# Usage 
+To use this daily planner to create a schedule, first, open the application.  The current day will be displayed at the top of the calendar.  When you scroll down, you will be presented with time blocks for standard business hours of 9am to 5pm.  View the time blocks for that day. Each time block is color-coded to indicate whether it is in the past, present, or future.  Click into a time block to enter an event.  When you click the save button for that time block, the text for that event is saved in local storage.  Refresh the page to see that the saved events persists.
+
+# Live Site Application
+[Link to Live Site](https://mcalvario.github.io/WorkDayCalendar/)
+[Screenshot of application](![Alt text](mycalendar.png))
+[Link to updated portfolio] https://mcalvario.github.io/Professional-Portfolio/
+
+# Credits
+Stackoverflow:  https://stackoverflow.com/questions/40791207/setting-and-getting-localstorage-with-jquery
+w3schools:  https://www.w3schools.com/jquery/traversing_siblings.asp
+youtube:  https://www.youtube.com/watch?v=AUOzvFzdIk4&t=80s
+
+# license
+
+Copyright (c) [2023] [Work-Day-Calendar]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- script.js
+++ script.js
@@ -1,57 +1,169 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var h9 = document.getElementById(".hour-9");
+var h10 = document.getElementById("hour-10");
+var h11 = document.getElementById("hour-11");
+var h12 = document.getElementById("hour-12");
+var hour1 = document.getElementById("hour-1");
+var hour2 = document.getElementById("hour-2");
+var hour3 = document.getElementById("hour-3");
+var hour4 = document.getElementById("hour-4");
+var hour5 = document.getElementById("hour-5");
+
+var h9description = document.querySelector(".h9description");
+//localStorage.setItem("server", input.val());
+
+var storedValue = localStorage.getItem("h9description");
+
+
+
+//var saveTextInput = h9
+
+//localStorage.setItem("h9", saveTextInput);
+
+//var h9 = localStorage.getItem("h9");
+
+//ar h9 = $(".hour-9");
+//h9.text("saveTextInput");
+
+//function getItem("h9");
+       // return Value;
+
+
+//<div id="hour-9" class="row time-block past">
+  //      <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+    //    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      //  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        //  <i class="fas fa-save" aria-hidden="true"></i>
+        //<button>
+      //</div>
+
+
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+  var description2 = $(this).siblings(".description2").val();
+  localStorage.setItem("textStorage2", description2.value);
+  (localStorage.getItem("textStorage2"));
+});
+
+      //var btn = document.querySelector('.btn')
+     //document.querySelector(".btn").addEventListener("click", function() {
+       // console.log("Hey!  You clicked the button!", localStorage);
+      //});
+
+
+      // var btn1 = document.querySelector('.btn1')
+      // document.querySelector(".btn1").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn2 = document.querySelector('.btn2')
+      // document.querySelector(".btn2").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn3 = document.querySelector('.btn3')
+      // document.querySelector(".btn3").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn4 = document.querySelector('.btn4')
+      // document.querySelector(".btn4").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn5 = document.querySelector('.btn5')
+      // document.querySelector(".btn5").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn7 = document.querySelector('.btn7')
+      // document.querySelector(".btn7").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn8 = document.querySelector('.btn8')
+      // document.querySelector(".btn8").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+      // var btn9 = document.querySelector('.btn9')
+      // document.querySelector(".btn9").addEventListener("click", function() {
+      //   console.log("Hey!  You clicked the button!");
+      // });
+
+//var btn = function() {
+ // text("saveTextInput" localStorage);
+//}
+
+
+
+
+     // var  = document.querySelector(".saveBtn");
+
+//saveBtn.addEventListener("click", function() {
+
+//console.log*("saveBtn")
+
+
+
+//h9.addEventListener("click", function() {
+  //localStorage.setItem("text", text);
+
+
+
+
+
+//var saveBtn9amEl = document.querySelector('#save-btn-9am')
+//hour9El.addEventListener("click", e => {
+  //console.log("Hour9")
+//})
+//saveBtn9amEl.addEventListener('click', (event) => {
+  //event.preventDefault()
+  //hour9El = document.querySelector('#hour-9 > textarea')
+ // localStorage.setItem('hour9am', hour9El.value)
+  //hour9El.textContent = localStorage.getItem('hour9am');
+//})
+
+
 $(function () {
+ 
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+    // useful when saving the description in local storage?  
+ 
+      
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-   
-    //
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    var hour9 = document.getElementById("hour-9");
-    var hour10 = document.getElementById("hour-10");
-    var hour11 = document.getElementById("hour-11");
-    var hour12 = document.getElementById("hour-12");
-    var hour1 = document.getElementById("hour-1");
-    var hour2 = document.getElementById("hour-2");
-    var hour3 = document.getElementById("hour-3");
-    var hour4 = document.getElementById("hour-4");
-    var hour5 = document.getElementById("hour-5");
 
-    //<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    saveBtnButton.addEventListener("click", function(event) {
-        event.preventDefault();
-   
-        var 
-
-
-
-
-
-
-
-
-
-
-    //
     // TODO: Add code to display the current date in the header of the page.
-//    var today = dayjs();
-//    $('#currentDay').text(today.format('MMM D, YYYY'));
-//});
-
-
-
 var today = dayjs();
     $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
 });
+
+
+//function renderLastRegistered() {
+  //var h9text = localStorage.getItem("h9text"); 
+
+ //textArea.textContent = h9text;
+//}
+
+//localStorage.setItem("h9text", h9text);
+
+//var h9text = h9textInput.value;
+
+//localStorage.setItem("h9text", h9text);
+//renderLastRegistered();
